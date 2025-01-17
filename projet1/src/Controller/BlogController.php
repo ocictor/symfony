f7a36cbea7e2c439df65fad121a4e39e4e272bc2
victor celarier <ocictor14@gmail.com>
2025-01-17 08:53:27 +0100

    votre message de commit

--- projet1/src/Controller/BlogController.php
+++ projet1/src/Controller/BlogController.php
@@ -8,20 +8,66 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'name' => 'CELARIER Victor',
+            'group' => 'A2',
+            'introduction' => 'Étudiant passionné en Réseaux et Télécommunications avec un fort intérêt pour la cybersécurité et l\'infrastructure cloud.'
         ]);
     }
 
-    #[Route('/blog/home', name: 'app_home')]
-    public function home(): Response
+    #[Route('/apropos', name: 'app_apropos')]
+    public function apropos(): Response
     {
-        return $this->render('blog/home.html.twig', [
-            'title' => 'bienvenue',
-            'age' => 31
+        return $this->render('blog/apropos.html.twig', [
+            'hobbies' => [
+                'Recherche en Cybersécurité',
+                'Architecture Réseau',
+                'Informatique en Cloud',
+                'Contribution Open Source'
+            ]
+        ]);
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('blog/cv.html.twig', [
+            'dreamJob' => 'Ingénieur Réseaux et Télécommunications',
+            'education' => [
+                [
+                    'degree' => 'BUT Réseaux et Télécommunications',
+                    'school' => 'IUT de Roanne',
+                    'year' => '2024-2025'
+                ],
+                [
+                    'degree' => 'Baccalauréat STI2D',
+                    'school' => 'Lycée de Mauriac',
+                    'year' => '2020-2024'
+                ]
+            ],
+            'skills' => [
+                'Protocoles Réseaux',
+                'Analyse de Sécurité',
+                'Infrastructure Cloud',
+                'Administration Système'
+            ]
+        ]);
+    }
+
+    #[Route('/portfolio', name: 'app_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('blog/portfolio.html.twig', [
+            'projects' => [
+                [
+                    'title' => 'Implémentation Sécurité Réseau',
+                    'description' => 'Mise en place d\'une infrastructure réseau sécurisée',
+                    'skills' => ['Sécurité', 'Réseaux', 'Documentation']
+                ]
+            ]
         ]);
     }
 }
